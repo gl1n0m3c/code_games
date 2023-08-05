56374f9fd5846352910112b90de61a87dffa4e2d
gl1n0m3c <nperewerzew@gmail.com>
2023-08-05 20:32:33 +0300

    sapper_start

--- sapper.py
+++ sapper.py
@@ -1,17 +1,10 @@
 import tkinter
 
 window = tkinter.Tk()  # window становится объектом класса (старт работы)
+window.geometry('400x400+100+200')
 
-ROW = 5
-COLOMNS = 7
 
-buttoms = []
-for i in range(ROW):
-    temp = []
-    for j in range(COLOMNS):
-        btn = tkinter.Button(window)
-        temp.append(btn)
-    buttoms.append(temp)
+
+
 
 window.mainloop()  # вызов метода mainloop (создание окошка)
-
